60c09ef5de2c0f2e93812a21d8766925d981b46b
DarshiBanan <banan.darshi@gmail.com>
2018-07-23 11:43:18 -0700

    Bring in cleaned leaf level traits (from REP 7/23), begin leaf-plant merge script.

---
+++ multiyear_analysis/Setaria_data_merge.R
@@ -0,0 +1,8 @@
+#Setaria_data_merge
+
+
+
+#merge plant and leaf level data 
+
+load("/de/github/dbanan/auth/Setaria_RIL_multiyear/data_wrangle/plant_level/data/harvest_phenotypes_clean_transformed.Rdata")
+
