e197208fa626f5f6b2fc18ba438c88aaab68151d
DarshiBanan <banan.darshi@gmail.com>
2018-07-27 13:37:37 -0700

    Merge leaf-plant.

--- multiyear_analysis/Setaria_data_merge.R
+++ multiyear_analysis/Setaria_data_merge.R
@@ -9,17 +9,15 @@ library(ggplot2)
 library(reshape2)
 
 #leaf
-leaf<-read.csv("../data_wrangle/leaf_level/Set_RIL_leaf_data_subplot_level.csv", header=T, stringsAsFactors=FALSE)
+#leaf<-read.csv("../data_wrangle/leaf_level/Set_RIL_leaf_data_subplot_level.csv", header=T, stringsAsFactors=FALSE)
 
-#still have some errant genotype names
-leaf$genotype[leaf$genotype==" A10"]<-"A10"
-leaf$genotype[leaf$genotype==" B100"]<-"B100"
-leaf$genotype[leaf$genotype=="RIL_60"]<-"RIL_060"
-leaf$genotype[leaf$genotype=="RIL_98"]<-"RIL_098"
-leaf$genotype[leaf$genotype=="RIL_72"]<-"RIL_072"
-leaf$genotype[leaf$genotype=="RIL_78"]<-"RIL_078"
-leaf$genotype[leaf$genotype=="RIL_18"]<-"RIL_018"
+#re-running with newest data version 
+load("../data_wrangle/leaf_level/leaf_phenotypes_clean.Rdata")
 
+leaf<-leaf.level
+#change "genotype" and "trait" from factor to character
+leaf$genotype<-as.character(as.factor(leaf$genotype))
+leaf$trait<-as.character(as.factor(leaf$trait))
 
 
 
@@ -31,7 +29,11 @@ plant<-all_stack2
 #leaf data lacks treatment column? 
 #so extract experimental design information from plant data? and merge with leaf data? 
 design_only<-unique(plant[,c(1:5)])
-leaf<-merge(leaf, design_only, by=c("year","experiment", "genotype", "subplot_id"), all=T)
+leaf<-merge(leaf, design_only, by=c("year","experiment", "genotype", "subplot_id"))
+
+#check back on this: why do we lose leaf data with this merge? 
+#Presumably there are observations taken from leaf samples that were not taken from plant samples? 
+
 
 #quick leaf level visualization 
 ggplot(leaf, aes(x=treatment, y=data, fill=treatment))+
@@ -50,10 +52,13 @@ ggplot(leaf, aes(x=treatment, y=data, fill=treatment))+
 lp<-rbind(leaf, plant)
 
 #large stretches of NA (missing data, especially in 2015)?
+#NaN for some plant traits across multiple years? 
+
+
 
 ####PRODUCT####
 save(lp, file="./merged_phenotypes.Rdata")
-write.csv(lp, file="./merged_phenotypes_clean.csv", row.names=FALSE)
+
 
 
 
