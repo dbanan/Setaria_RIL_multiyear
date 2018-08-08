cc4c58ad2f358bc30f29c5d5221fb9e6707f2f9a
DarshiBanan <banan.darshi@gmail.com>
2018-08-08 16:29:26 -0700

    Run PCA with trimmed variable set.

--- supplemental_analysis/environmental_data/scripts/Setaria_environmental_drivers.R
+++ supplemental_analysis/environmental_data/scripts/Setaria_environmental_drivers.R
@@ -22,7 +22,20 @@ load("./results/weather_data_all.Rdata")
 #####PCA#####
 #environments of interest 
 
-all_env<-weather_all[-c(20,24)]
+all_env<-weather_all
+all_env$name<-paste(all_env$environment, all_env$DAS, sep="_")
+
+#trim to variables of interest 
+all_env<-all_env[,c(38,25,28,2,6,10,5,23,33,34)]
+
+all_env1<-all_env[complete.cases(all_env),]
+
+env<-all_env1[,2]
+
+rownames(all_env1)<-all_env1$name
+
+
+
 
 #variables of interest 
 just_wide1<-all_env[complete.cases(all_env),]
@@ -31,12 +44,14 @@ just_wide1<-all_env[complete.cases(all_env),]
 env<-just_wide1[,23]
 
 #join genotype and environment
-just_wide1$name<-paste(just_wide1$environment, just_wide1$calendar, sep="_")
+just_wide1$name<-paste(just_wide1$environment, just_wide1$DAS, sep="_")
 rownames(just_wide1)<-just_wide1$name
 just_wide1$name<-NULL
 
+#trim data to variables of interest for PCA
 just_wide2<-just_wide1[,c(2:22,24:27)]
 
+just_wide2<-all_env1[,c(4:10)]
 
 pca_try<-prcomp(just_wide2, center=TRUE, scale.=TRUE)
 
@@ -66,6 +81,9 @@ ggbiplot(pca_try, choices=c(1,3),obs.scale=1, var.scale=1, groups=env, ellipse=T
 ggbiplot(pca_try, choices=c(2,3),obs.scale=1, var.scale=1, groups=env, ellipse=TRUE)
 
 
+ggbiplot(pca_try, choices=c(1,4),obs.scale=1, var.scale=1, groups=env, ellipse=TRUE)
+ggbiplot(pca_try, choices=c(2,4),obs.scale=1, var.scale=1, groups=env, ellipse=TRUE)
+ggbiplot(pca_try, choices=c(3,4),obs.scale=1, var.scale=1, groups=env, ellipse=TRUE)
 
 
 #####PCA OVER TIME#####
@@ -81,16 +99,16 @@ compl<-melt(comp, id.vars=c("name"), measure.vars=c("PC1","PC2","PC3"), variable
 compl$data<-as.numeric(compl$data)
 
 #break "name" into plottable attributes 
-compl<-separate(compl, name, into=c("treatment","year","calendar"), sep="_")
+compl<-separate(compl, name, into=c("treatment","year","DAS"), sep="_")
 compl$environment<-paste(compl$treatment, compl$year, sep="_")
-compl$calendar<-as.Date(compl$calendar)
+compl$DAS<-as.numeric(compl$DAS)
 
 
 
 
-ggplot(compl, aes(x=calendar, y=data, group=component, color=component))+
+ggplot(compl, aes(x=DAS, y=data, group=component, color=component))+
   geom_smooth()+
-  facet_wrap(~environment, scale="free")
+  facet_wrap(~environment)
 
 
 
